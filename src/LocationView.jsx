48486a759cb264c1f69e3b673318a7b7de1b9c4c
MrManBLC <gman.dude132@gmail.com>
2022-04-28 11:53:03 -0400

    Update LocationView visuals

--- src/LocationView.jsx
+++ src/LocationView.jsx
@@ -25,22 +25,24 @@ function LocationView(props){
   const imageIds = location.images;
 
   return(
-    <div className="rounded-xl mx-auto mb-20 w-9/12 content-center bg-blue-500 p-4" style={{ height: 800 }} ref={fieldRef}>
+    <div className="rounded-xl mx-auto mb-20 w-9/12 content-center bg-gray-100 p-4 shadow-2xl" style={{ height: 800 }} ref={fieldRef}>
       <div className="grid grid-cols-7 gap-4" style={{height: "100%"}}>
-        <div className="bg-opacity-25 col-span-4 row-span-2 rounded-xl flex items-center relative">
+        <div className="col-span-4 row-span-2 rounded-xl flex items-center relative">
           <img src={`http://localhost:5000/getImage/${imageIds[0]}`}
             style={{WebkitFilter:"blur(8px)", objectFit:"cover"}}
             className="h-full rounded-xl"/>
           <div className="p-4 absolute">
             <img src={`http://localhost:5000/getImage/${imageIds[0]}`}
-              className="rounded-xl"/>
+              className="rounded-xl shadow-2xl"/>
           </div>
         </div>
-        <div className="bg-black bg-opacity-25 col-span-3 py-5 px-2 rounded-xl">
-          { name } | Rating: { rating } <br/>
-          { description } <br/>
+        <div className="bg-gray-200 col-span-3 py-5 px-2 rounded-xl shadow-inner">
+          <div className="flex space-x-4">
+            <p className="text-lg text-black font-bold">{name}</p> <p className="text-lg">Rating: {rating} </p>
+          </div>
+          <p>{description}</p>
         </div>
-        <div className="bg-black bg-opacity-25 col-span-3 py-5 px-2 rounded-xl">
+        <div className="bg-gray-200 col-span-3 py-5 px-2 rounded-xl shadow-inner">
           TODO: Ratings
         </div>
       </div>
