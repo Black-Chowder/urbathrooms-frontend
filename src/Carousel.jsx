6467a4d60b1892a6badfd1222883726daa87c525
MrManBLC <gman.dude132@gmail.com>
2022-04-28 22:43:52 -0400

    Carousel navigation updated

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-export const CarouselItem = ({ children, width }) => {
+const CarouselItem = ({ children }) => {
   return(
     <div className="h-full" style={{display:"inline-flex", alignItems:"center", justifyContent:"center"}}>
       {children}
@@ -18,10 +18,13 @@ const Carousel = props => {
     children.push(
       <CarouselItem className="h-full">
         <div className="relative flex items-center justify-center rounded-xl h-full">
+          {/* Blurry Background Image */}
           <img src={src}
             style={{ WebkitFilter: "blur(8px)", objectFit: "cover" }}
             className="h-full rounded-xl" />
-          <div className="p-4 absolute">
+
+          {/* Actual Image */}
+          <div className="p-9 absolute">
             <img src={src}
               className="rounded-xl shadow-2xl"
               alt="Preview Image"/>
@@ -43,30 +46,50 @@ const Carousel = props => {
 
   return(
     <div className="carousel h-full rounded-xl shadow-inner relative" style={{ overflow:"hidden" }}>
-      <div className="inner h-full" style={{ transform: `translateX(-${displayIndex}00%)`, whiteSpace:"nowrap", transition:"transform 0.3s" }}>
+
+      {/* Display Image Handling */}
+      <div className="inner h-full" 
+        style={{ transform: `translateX(-${displayIndex}00%)`, whiteSpace:"nowrap", transition:"transform 0.3s" }}>
         {React.Children.map(children, (child, index) => {
           return React.cloneElement(child, { width: "100%" });
         })}
       </div>
+
+      {/* Left and right navigation */}
+      <div className="absolute top-0 h-full w-full flex">
+        <div className="flex my-auto w-full h-full justify-between py-14">
+          <div className="hover:bg-black hover:bg-opacity-20 flex justify-between cursor-pointer rounded-r-xl" 
+            onClick={() => { updateIndex(displayIndex - 1); }}>
+            <p className="text-white font-extrabold text-5xl my-auto">
+                {"<"}
+            </p>
+          </div>
+          <div className="hover:bg-black hover:bg-opacity-20 flex justify-between cursor-pointer rounded-l-xl" 
+            onClick={() => { updateIndex(displayIndex + 1); }}>
+            <p className="text-white font-extrabold text-5xl my-auto"
+              >
+              {">"}
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {/* Indexed image navigation */}
       <div className="indicators absolute bottom-0 w-full">
         <div className="flex justify-center space-x-2">
-          <button className="text-white font-bold" onClick={() => { updateIndex(displayIndex - 1); }}>
-            Previous
-          </button>
           {
             React.Children.map(children, (child, index) => {
               return(
+                //TODO: Change to display miniature images
                 <button className="text-white font-bold" onClick={() => { updateIndex(index); }}>
                   {index + 1}
                 </button>
-              )
+              );
             })
           }
-          <button className="text-white font-bold" onClick={() => { updateIndex(displayIndex + 1); }}>
-            Next
-          </button>
         </div>
       </div>
+
     </div>
   );
 }
--- src/LocationView.jsx
+++ src/LocationView.jsx
@@ -1,6 +1,6 @@
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { BACKEND_URI } from './GlobalConsts';
-import Carousel, { CarouselItem } from './Carousel';
+import Carousel from './Carousel';
 
 function LocationView(props){
   const location = props.location;
@@ -34,20 +34,7 @@ function LocationView(props){
     <div className="rounded-xl mx-auto mb-20 w-9/12 content-center bg-gray-100 p-4 shadow-2xl" style={{ height: 800 }} ref={fieldRef}>
       <div className="grid grid-cols-7 gap-4" style={{height: "100%"}}>
         <div className="col-span-4 row-span-2 rounded-xl flex items-center">
-
-          <Carousel
-            srcs={imageSources}>
-          </Carousel>
-
-          {/* 
-          <img src={`${BACKEND_URI}getImage/${imageIds[0]}`}
-            style={{WebkitFilter:"blur(8px)", objectFit:"cover"}}
-            className="h-full rounded-xl"/>
-          <div className="p-4 absolute">
-            <img src={`${BACKEND_URI}getImage/${imageIds[0]}`}
-              className="rounded-xl shadow-2xl"/>
-          </div>
-          */}
+          <Carousel srcs={imageSources}/>
         </div>
         <div className="bg-gray-200 col-span-3 py-5 px-2 rounded-xl shadow-inner">
           <div className="flex space-x-4">
